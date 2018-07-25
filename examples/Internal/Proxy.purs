60218c85d63fe093a31fd80e3508e68d0cf47e12
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-07-25 15:24:22 -0700

    Update examples.

--- examples/Internal/Proxy.purs
+++ examples/Internal/Proxy.purs
@@ -1,6 +1,5 @@
 -- | A proxy that hides both the Query and Message of wrapped component.
 -- | Adapted from `Halogen.Component.Proxy` and `Halogen.Storybook.Proxy`.
-
 module Docs.Internal.Proxy
   ( ProxyS
   , proxy
@@ -12,41 +11,37 @@ import Data.Const (Const(..))
 import Data.Coyoneda (Coyoneda, unCoyoneda)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (un)
-import Data.Symbol (class IsSymbol, SProxy(..))
+import Data.Symbol (SProxy(..))
 import Halogen as H
 import Halogen.HTML as HH
-import Prim.Row as Row
 
 data ProxyS f i a
   = Query (Coyoneda f a)
 
 -- | A proxy that hides both the Query and Message of wrapped component.
 proxy
-  :: ∀ f i o m
-   . H.Component HH.HTML f i o m
+  :: forall f i o m
+  . H.Component HH.HTML f i o m
   -> H.Component HH.HTML (ProxyS (Const Void) i) i Void m
-proxy = proxyEval (const (absurd <<< un Const)) (SProxy :: SProxy "slot")
+proxy = proxyEval (const (absurd <<< un Const))
 
 proxyEval
-  :: ∀ f g i o cs m t0 sym
-   . IsSymbol sym
-  => Row.Cons sym (H.Slot f o Unit) t0 cs
-  => (∀ a b. (b -> a) -> g b -> H.HalogenM i (ProxyS g i) cs Void m a)
-  -> SProxy sym
+  :: forall f g i o m
+   . (forall a b. (b -> a) -> g b -> H.HalogenM i (ProxyS g i) (child :: H.Slot f o Unit) Void m a)
   -> H.Component HH.HTML f i o m
   -> H.Component HH.HTML (ProxyS g i) i Void m
-proxyEval evalQuery sym component =
+proxyEval evalQuery component =
   H.component
     { initialState: identity
-    , render: render
+    , render
     , eval
     , receiver: const Nothing
     , initializer: Nothing
     , finalizer: Nothing
     }
   where
-    render :: i -> H.ComponentHTML (ProxyS g i) cs m
-    render i = HH.slot sym unit component i (const Nothing)
+    render :: i -> H.ComponentHTML (ProxyS g i) (child :: H.Slot f o Unit) m
+    render i = HH.slot (SProxy :: SProxy "child") unit component i (const Nothing)
 
-    eval :: ProxyS g i ~> H.HalogenM i (ProxyS g i) cs Void m
+    eval :: ProxyS g i ~> H.HalogenM i (ProxyS g i) (child :: H.Slot f o Unit) Void m
     eval (Query iq) = unCoyoneda evalQuery iq
--- examples/Main.purs
+++ examples/Main.purs
@@ -2,31 +2,30 @@ module Main where
 
 import Prelude
 
-import Effect.Aff (Aff)
-import Effect.Aff.Class (class MonadAff)
-import Effect (Effect)
-import Effect.Class (liftEffect)
-import Web.HTML.HTMLElement (HTMLElement, toElement, fromNode)
-import Web.HTML (window)
-import Web.HTML.Window (document)
-import Web.HTML.HTMLDocument (toParentNode)
-import Web.DOM.Element (getAttribute)
-import Web.DOM.NodeList (toArray)
-import Web.DOM.ParentNode (QuerySelector(..), querySelectorAll)
 import Data.Array (zipWith)
 import Data.Const (Const)
 import Data.Map as Map
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Symbol (SProxy(..))
 import Data.Traversable (sequence, traverse, traverse_)
 import Data.Tuple (Tuple(..))
-
+import Docs.Internal.Component as Component
+import Docs.Internal.Proxy (ProxyS, proxy)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (liftEffect)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.VDom.Driver (runUI)
-
-import Docs.Internal.Proxy (ProxyS, proxy)
-import Docs.Internal.Component as Component
+import Web.DOM.Element (getAttribute)
+import Web.DOM.NodeList (toArray)
+import Web.DOM.ParentNode (QuerySelector(..), querySelectorAll)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toParentNode)
+import Web.HTML.HTMLElement (HTMLElement, toElement, fromNode)
+import Web.HTML.Window (document)
 
 -- Finds all nodes labeled "data-component-id" and retrieves the associated attribute.
 -- Then, mounts the right component at each node.
@@ -65,12 +64,12 @@ app =
     }
   where
     render st = do
-      let (Tuple sym mbComponent) = Map.lookup st routes
+      let mbComponent = Map.lookup st routes
       case mbComponent of
         Nothing -> HH.div_ []
-        Just component -> HH.slot sym unit component unit absurd
+        Just component -> HH.slot (SProxy :: SProxy "child") unit component unit absurd
 
-    eval :: Query ~> H.HalogenM String Query _ Void m
+    eval :: Query ~> H.HalogenM String Query (child :: H.Slot (ProxyS (Const Void) Unit) Void Unit) Void m
     eval (NoOp a) = pure a
 
 
